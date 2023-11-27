56b2084710f6a34252ec1872d9245e7e06307699
Anurag <anurag@ph.iitr.ac.in>
2023-11-28 02:58:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# instagram-post-scraper
+# Instagram Post Scraper
+This Python script utilizes the Instaloader library to scrape likes and comments from Instagram posts. The script takes Instagram credentials and a list of post links as command-line arguments, then outputs the data to a CSV file.
+
+## Requirements
+Python 3.x
+Instaloader library (pip install instaloader)
+Pandas library (pip install pandas)
+Usage
+Clone the repository:
+
+bash
+
+git clone https://github.com/yourusername/instagram-post-scraper.git
+cd instagram-post-scraper
+Install the required libraries:
+
+bash
+
+Copy code
+pip install -r requirements.txt
+
+Run the script with the following command:
+
+bash
+Copy code
+python scraper.py <username> <password> <post_link1> <post_link2> ...
+Replace <username> and <password> with your Instagram credentials, and <post_link1>, <post_link2>, etc., with the Instagram post links you want to scrape.
+
+The script will output the results to a file named instagram.csv.
+
+Command Line Arguments
+username: Your Instagram username.
+password: Your Instagram password.
+links: List of Instagram post links to scrape.
+Output
+The script will generate a CSV file (instagram.csv) containing the following columns:
+
+Platform: The social media platform (always "instagram" in this case).
+Post: The URL of the Instagram post.
+user: List of users who liked or commented on the post.
+useraction: The type of user action ("like" or "comment").
+Important Note
+Please be aware of Instagram's rate limits when using this script. The script incorporates a basic rate-limiting mechanism to avoid exceeding the allowed number of requests per hour. If the rate limit is reached, the script will pause execution to comply with Instagram's policies.
+
+## Disclaimer
+This script is intended for educational purposes and personal use only. Make sure you comply with Instagram's terms of service and data usage policies when using this script. The developers are not responsible for any misuse or violations.
---
+++ insta.py
@@ -0,0 +1,95 @@
+import instaloader
+import pandas as pd
+import re
+import argparse
+import time
+
+parser = argparse.ArgumentParser(description='Running Instagram Post Scrawler')
+
+parser.add_argument('username', type=str, help='username')
+parser.add_argument('password', type=str, help='password')
+parser.add_argument('links', nargs='+', type=str, help='list of links')
+
+args = parser.parse_args()
+
+def extract_end_id(link):
+    # Define a regular expression pattern to match the end ID
+    pattern = r'https://www\.instagram\.com/p/([a-zA-Z0-9_-]+)'
+    
+    # Use re.search to find the match in the link
+    match = re.search(pattern, link)
+    
+    # Check if a match is found
+    if match:
+        # Extract and return the end ID
+        return match.group(1)
+    else:
+        # Return None if no match is found
+        return None
+
+
+Posts = [extract_end_id(link) for link in args.links]
+print(Posts)
+
+
+df = pd.DataFrame(columns=['Platform', 'Post', 'user','useraction'])
+L = instaloader.Instaloader()
+L.login(args.username,args.password)
+#L.login('testing1232260', 'Durga@123')
+requests_made = 0
+rate_limit = 200
+start_time = time.time()
+
+for post_id in Posts:
+    Post = instaloader.Post.from_shortcode(L.context, post_id)
+    user=[]
+    for like in Post.get_likes():
+        user.append(like.username)
+        requests_made += 1
+        
+        if requests_made >= rate_limit:
+            # Calculate the time elapsed
+            elapsed_time = time.time() - start_time
+            print(len(user))
+
+            # If the time elapsed is less than an hour, sleep to stay within the rate limit
+            if elapsed_time < 3600:
+                sleep_time = 3600 - elapsed_time
+                print(f"Rate limit reached. Sleeping for {sleep_time} seconds.")
+                time.sleep(sleep_time)
+
+            # Reset the variables for the next hour
+            start_time = time.time()
+            requests_made = 0
+        
+    new_row = {'Platform': 'instagram', 'Post': f'https://www.instagram.com/p/{post_id}','user':user,'useraction':'like'}
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+'''
+requests_made = 0
+rate_limit = 200
+start_time = time.time()   
+for post_id in Posts:
+    Post = instaloader.Post.from_shortcode(L.context, post_id)
+    
+    post_comments = Post.get_comments()
+    for comment in post_comments:
+        user_comments.append(comment.owner.username)
+        requests_made += 1
+        if requests_made >= rate_limit:
+            # Calculate the time elapsed
+            elapsed_time = time.time() - start_time
+
+            # If the time elapsed is less than an hour, sleep to stay within the rate limit
+            if elapsed_time < 3600:
+                sleep_time = 3600 - elapsed_time
+                print(f"Rate limit reached. Sleeping for {sleep_time} seconds.")
+                time.sleep(sleep_time)
+
+            # Reset the variables for the next hour
+            start_time = time.time()
+            requests_made = 0
+    new_row = {'Platform': 'instagram', 'Post': f'https://www.instagram.com/p/{post_id}','user':user_comments,'useraction':'comment'}
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+'''
+df.to_csv('instagram.csv')
